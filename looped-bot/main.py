a6c1035d4aa5f0335cf1c0fce4bebb0d1a4b0354
Egor Dubrovin <dubrovin.en@ya.ru>
2022-09-17 00:32:36 +0300

    Автоматическое восстановление бота после ошибочного падения

--- looped-bot/main.py
+++ looped-bot/main.py
@@ -1,4 +1,19 @@
 import asyncio
+import traceback
+
 from bot import Bot
 
-asyncio.run(Bot.infinity_polling())
+async def bot_restart_sequence():
+    while True:
+        try:
+            await Bot.infinity_polling()
+        except asyncio.CancelledError:
+            # don't interfere with cancellations
+            raise
+        except Exception:
+            print("Caught exception")
+            traceback.print_exc()
+
+if __name__ == "__main__":
+    print("Well, starting night cock...")
+    asyncio.run(bot_restart_sequence())
