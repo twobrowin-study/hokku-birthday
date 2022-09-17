75f608be71ab4743552a2b46d20bf30da5f245e5
Egor Dubrovin <dubrovin.en@ya.ru>
2022-09-17 22:16:31 +0300

    Попытка изменения таймаута запросов

--- looped-bot/main.py
+++ looped-bot/main.py
@@ -1,19 +1,8 @@
 import asyncio
-import traceback
+import logging
 
 from bot import Bot
 
-async def bot_restart_sequence():
-    while True:
-        try:
-            await Bot.infinity_polling()
-        except asyncio.CancelledError:
-            # don't interfere with cancellations
-            raise
-        except Exception:
-            print("Caught exception")
-            traceback.print_exc()
-
 if __name__ == "__main__":
     print("Well, starting night cock...")
-    asyncio.run(bot_restart_sequence())
+    asyncio.run(Bot.infinity_polling(timeout=1, logger_level=logging.INFO))
