d5b225efb65a5113920274aa6a22ae33d96a3656
Egor Dubrovin <dubrovin.en@ya.ru>
2022-09-19 10:12:15 +0300

    Изменена библиотека для looped-bot

--- looped-bot/bot.py
+++ looped-bot/bot.py
@@ -1,14 +1,14 @@
-from telebot.async_telebot import AsyncTeleBot
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes
+from telegram.ext.filters import TEXT
 
 from datetime import datetime
 from settings import BotToken
 
-Bot = AsyncTeleBot(BotToken, parse_mode='Markdown')
+async def no_reaction_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print("\n{}: Got message from".format(datetime.now()))
+    print(f"From {update.effective_user.id} aka {update.effective_user.name} in {update.effective_chat.id} witch is {update.effective_chat.title}")
+    await update.message.reply_markdown("На таких петушар как ты я не реагирую!")
 
-@Bot.message_handler()
-async def no_reaction_handler(message):
-    print("\n{}: Got message".format(datetime.now()))
-    print(message)
-    await Bot.send_message(message.chat.id, """
-На таких петушар как ты я не реагирую!
-    """)
+Bot = ApplicationBuilder().token(BotToken).build()
+Bot.add_handler(MessageHandler(TEXT, no_reaction_handler))
--- looped-bot/main.py
+++ looped-bot/main.py
@@ -1,8 +1,5 @@
-import asyncio
-import logging
-
 from bot import Bot
 
 if __name__ == "__main__":
     print("Well, starting night cock...")
-    asyncio.run(Bot.infinity_polling(timeout=1, logger_level=logging.INFO))
+    Bot.run_polling()
