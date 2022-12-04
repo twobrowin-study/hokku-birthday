f85725226660ce61131d11a19a4c3322ba1d233c
Egor Dubrovin <dubrovin.en@ya.ru>
2022-12-05 00:45:49 +0300

    Добавлен фильтр исключительно приватных сообщений

--- looped-bot/bot.py
+++ looped-bot/bot.py
@@ -1,9 +1,14 @@
-from telegram import Update
+from telegram import Update, Message
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes
-from telegram.ext.filters import ALL
+from telegram.ext.filters import MessageFilter
 
 from datetime import datetime
 from settings import BotToken
+
+class PrivateChatClass(MessageFilter):
+    def filter(self, message: Message) -> bool:
+        return message.chat.type == 'private'
+PrivateChatFilter = PrivateChatClass()
 
 async def no_reaction_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     print("\n{}: Got message from".format(datetime.now()))
@@ -11,4 +16,4 @@ async def no_reaction_handler(update: Update, context: ContextTypes.DEFAULT_TYPE
     await update.message.reply_markdown("На таких петушар как ты я не реагирую!")
 
 Bot = ApplicationBuilder().token(BotToken).build()
-Bot.add_handler(MessageHandler(ALL, no_reaction_handler))
+Bot.add_handler(MessageHandler(PrivateChatFilter, no_reaction_handler))
